6ed185a38eabf747268716694a5130d89238b2da
Kiires <nesterovaes04@mail.ru>
2022-10-20 00:25:39 +0300

    lfs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,3 @@
-//
-// Created by Екатерина on 19.10.2022.
-//
-
-#include <iostream>
-using namespace std;
-int main() {
-    cout << "Hello, my dear friend!" << "\n";
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e60fcac0855ee17f3c1354b65992c4ceb85f8479386a031b442fda5db81dcf03
+size 157
